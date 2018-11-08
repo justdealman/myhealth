3d06c415e61ac2781f53f28ffb82570b45da54d6
justdealman <justdealman@gmail.com>
2018-11-08 16:32:51 +0700

    Initial commit

---
+++ dist/js/scripts.js
@@ -0,0 +1,95 @@
+function setImgCover(e) {
+	e.each(function() {
+		$(this).parent().css({
+			'background-image': 'url("'+$(this).attr('src')+'")',
+			'background-repeat': 'no-repeat',
+			'background-position': 'center center',
+			'background-size': 'cover'
+		});
+	});
+}
+function setImgContain(e) {
+	e.each(function() {
+		$(this).parent().css({
+			'background-image': 'url("'+$(this).attr('src')+'")',
+			'background-repeat': 'no-repeat',
+			'background-position': 'center center',
+			'background-size': 'contain'
+		});
+	});
+}
+function setRatio() {
+	$('[data-ratio]').each(function() {
+		var t = $(this).find('.scale');
+		if ( !t.is('.scale-min') ) {
+			t.outerHeight(t.outerWidth()*$(this).attr('data-ratio'));
+		} else {
+			t.css({
+				'min-height': t.outerWidth()*$(this).attr('data-ratio')
+			});
+		}
+	});
+}
+$(function() {
+	setImgCover($('.img-cover'));
+	setImgContain($('.img-contain'));
+	var isMobile = false;
+	var justSwitched = false;
+	function detectDevice() {
+		var temp = isMobile;
+		if ( Modernizr.mq('(max-width:991px)') ) {
+			isMobile = true;
+		} else {
+			isMobile = false;
+		}
+		if ( temp == isMobile ) {
+			justSwitched = false;
+		} else {
+			justSwitched = true;
+		}
+	}
+	
+	$('.hero').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+		infinite: true,
+		speed: 500,
+		autoplay: true,
+		autoplaySpeed: 3000
+	});
+	
+	function startApp() {
+		detectDevice();
+		if ( justSwitched ) {
+			if ( isMobile ) {
+				
+			} else {
+				
+			}
+		}
+		setRatio();
+	}
+	
+	startApp();
+	var lastWidth = $(window).width();
+	$(window).on('resize', _.debounce(function() {
+		if ( $(window).width() != lastWidth ) {
+			startApp();
+			lastWidth = $(window).width();
+		}
+	}, 100));
+	
+
+
+	$('input, textarea').each(function() {
+		$(this).data('holder', $(this).attr('placeholder'));
+		$(this).focusin(function() {
+			$(this).attr('placeholder', '');
+		});
+		$(this).focusout(function() {
+			$(this).attr('placeholder', $(this).data('holder'));
+		});
+	});
+});
